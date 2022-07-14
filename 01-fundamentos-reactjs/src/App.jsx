ec3d2e586be92e9ca144aea1efe20cc0c254b70a
Lais Golin <laisgolin@gmail.com>
2022-07-14 17:01:30 +0100

    first commit

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import {Post} from './Post';
+
+export function App() {
+
+  return (
+    <div>
+      <Post
+      author="Lais Golin"
+      content="lorem ipsum dolor sit amet, consectetur adip"
+      />
+  <Post
+    author="Sina Stiller"
+    content="Teste numero 2"
+  />
+</div>
+  )
+}
