d4ff609e92b519bafe75bfddf93f8939668f5e7b
Lais Golin <laisgolin@gmail.com>
2022-07-14 20:43:33 +0100

    Add Header to the application

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,12 @@
+import {Header} from './components/Header.jsx';
 import {Post} from './Post';
+import './global.css';
 
 export function App() {
 
   return (
     <div>
+      <Header />
       <Post
       author="Lais Golin"
       content="lorem ipsum dolor sit amet, consectetur adip"
