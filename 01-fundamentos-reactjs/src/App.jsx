b028f3191d7c992f66975534fdfa300574a6c478
Lais Golin <laisgolin@gmail.com>
2022-07-15 10:31:25 +0100

    Add Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,25 +1,34 @@
 import {Header} from './components/Header.jsx';
 import {Post} from './Post';
 import './global.css';
+import styles from './App.module.css';
+import {Sidebar} from "./components/Sidebar.jsx";
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post
-      author="Lais Golin"
-      content="lorem ipsum dolor sit amet, consectetur adip"
-      />
-  <Post
-    author="Sina Stiller"
-    content="Teste numero 2"
-  />
 
-      <Post
-        author="Thais Golin"
-        content="Teste numero 3"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lais Golin"
+            content="lorem ipsum dolor sit amet, consectetur adip"
+          />
+          <Post
+            author="Sina Stiller"
+            content="Teste numero 2"
+          />
+
+          <Post
+            author="Thais Golin"
+            content="Teste numero 3"
+          />
+        </main>
+
+      </div>
 </div>
   )
 }
