b176e98abac5b7bd1ccb134ad8a5be4939180800
Lais Golin <laisgolin@gmail.com>
2022-07-16 17:35:18 +0100

    avatar

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import {ThumbsUp, Trash} from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/lgolin.png"/>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lais Golin</strong>
+              <time title="17 de Julho às 9:44h" dateTime="2022-15-07 09:44:00">Cerca de 1h atrás</time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+      
+
+      <footer>
+        <button>
+          <ThumbsUp/>
+          Aplaudir <span>20</span>
+        </button>
+      </footer>
+      </div>
+    </div>
+  )
+}
