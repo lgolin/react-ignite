6030c47b431995568011b639dacb874ffb57b21c
Lais Golin <laisgolin@gmail.com>
2022-09-07 17:40:06 +0100

    styling header

--- projetos/01-praticando-os-conceitos-do-reactjs/src/App.tsx
+++ projetos/01-praticando-os-conceitos-do-reactjs/src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="Diego Fernandes" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. " />
 
       <Post author="Lais Golin" content="Eaque numquam ullam a nulla totam labore officia" />
