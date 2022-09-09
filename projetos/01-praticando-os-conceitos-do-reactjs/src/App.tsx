bdd83dd6509ac6e5a1d87f32ebcd782a30db1fe5
Lais Golin <laisgolin@gmail.com>
2022-09-09 14:13:32 +0100

    Add search box and button

--- projetos/01-praticando-os-conceitos-do-reactjs/src/App.tsx
+++ projetos/01-praticando-os-conceitos-do-reactjs/src/App.tsx
@@ -1,5 +1,5 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
+import { Search } from './components/Search';
 
 import './global.css';
 
@@ -7,9 +7,7 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post author="Diego Fernandes" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. " />
-
-      <Post author="Lais Golin" content="Eaque numquam ullam a nulla totam labore officia" />
+      <Search />
     </div>
   )
 }
