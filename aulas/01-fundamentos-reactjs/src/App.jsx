1cd89d2fd24b6398647ee90ffcfb70fac040f574
Lais Golin <laisgolin@gmail.com>
2022-10-12 20:56:31 +0100

    creating a new app all over again

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Post } from './Post';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Post
+        author="Lais Golin"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      />
+      <Post
+        author="Sina Stiller"
+        content="Hi there, my name is Sina and I am new here"
+      />
+      <Post />
+      <Post />
+    </>
+  );
 }
-
-export default App
---
+++ aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Post({ author, content }) {
+  return (
+    <>
+      <h2>{author}</h2>
+      <p>{content}</p>
+    </>
+  );
+}
