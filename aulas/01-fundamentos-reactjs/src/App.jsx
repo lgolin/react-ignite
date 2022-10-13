bfd404429330f4ddefd2bedde38db3ac173e1a64
Lais Golin <laisgolin@gmail.com>
2022-10-13 12:12:09 +0100

    add sidebar

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,18 +1,28 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import styles from './App.module.css';
+import './global.css';
+import Sidebar from './components/Sidebar';
 
 export function App() {
   return (
     <>
-      <Post
-        author="Lais Golin"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-      <Post
-        author="Sina Stiller"
-        content="Hi there, my name is Sina and I am new here"
-      />
-      <Post />
-      <Post />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lais Golin"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post
+            author="Sina Stiller"
+            content="Hi there, my name is Sina and I am new here"
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export default function Sidebar() {
+  return (
+    <div>
+      <aside className={styles.sidebar}>
+        <img
+          src="https://images.unsplash.com/photo-1665583243409-671149263433?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+          className={styles.cover}
+        />
+        <div className={styles.profile}>
+          <img className={styles.avatar} src="https://github.com/lgolin.png" />
+          <strong>Lais Golin</strong>
+          <span>Web Developer</span>
+        </div>
+        <footer>
+          <a href="#">
+            <PencilLine size={20} /> Editar seu perfil
+          </a>
+        </footer>
+      </aside>
+    </div>
+  );
+}
